a00d92142eedefd5ba5622577d3559961e4fcbfa
aruprakshit <aruprakshit@rocketmail.com>
2024-05-21 17:52:59 +0530

    Added the implementation logic to the method to make sure all testcases passes

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,10 @@
-def add(string)
+def add(string, delimt = ',')
   if string.empty?
     0
   elsif string.length == 1
     string.to_i
+  else
+    a, b = string.split(delimt)
+    a.to_i + b.to_i
   end
 end
