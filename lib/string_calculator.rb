44657050dddc2a20c42bf59d7e798c6529e3735c
aruprakshit <aruprakshit@rocketmail.com>
2024-05-21 18:56:25 +0530

    refactor string to array conversion logic into its own method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,16 @@
-def add(string, delimt = ',')
+def add(string)
   if string.empty?
     0
   elsif string.length == 1
     string.to_i
   else
-    string.split(delimt).reduce(0) { |sum, n| sum + n.to_i }
+    string_to_array(string).reduce(0) { |sum, n| sum + n.to_i }
   end
+end
+
+private
+
+def string_to_array(string)
+  delimt = ","
+  string.split(delimt)
 end
